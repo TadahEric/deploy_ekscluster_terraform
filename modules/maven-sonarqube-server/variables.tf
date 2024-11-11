c7b28ab91fb8bb81fd74241a0128353a54e33207
Tadaheric <eric.tadah88@gmail.com>
2024-11-11 17:42:46 +0000

    update

--- modules/maven-sonarqube-server/variables.tf
+++ modules/maven-sonarqube-server/variables.tf
@@ -64,4 +64,5 @@ variable "db_security_group_id" {
 variable "db_subnet_group" {
   description = "The subnet group for the PostgreSQL database"
   type        = string
+  default = ["subnet-05a1d237ea47c7733", "subnet-06912fcda224d0ae1"]
 }
