2d90b6197b31872643ac9b634a428bd55b9e70b1
Tadaheric <eric.tadah88@gmail.com>
2024-11-11 18:31:29 +0000

    update

--- main.tf
+++ main.tf
@@ -79,17 +79,17 @@ module "aws_alb_controller" {
 # }
 
 
-module "maven-sonarqube-server" {
-  source            = "./modules/maven-sonarqube-server"
-  ami_id            = var.ami_id
-  instance_type     = var.instance_type
-  key_name          = var.key_name
-  security_group_id = var.security_group_id
-  subnet_id         = [var.subnet_id]
-  #main-region   = var.main-region
-  db_name              = var.db_name
-  db_username          = var.db_username
-  db_password          = var.db_password
-  db_subnet_group      = [var.db_subnet_group]
-  db_security_group_id = var.db_security_group_id 
-}
+# module "maven-sonarqube-server" {
+#   source            = "./modules/maven-sonarqube-server"
+#   ami_id            = var.ami_id
+#   instance_type     = var.instance_type
+#   key_name          = var.key_name
+#   security_group_id = var.security_group_id
+#   subnet_id         = [var.subnet_id]
+#   #main-region   = var.main-region
+#   db_name              = var.db_name
+#   db_username          = var.db_username
+#   db_password          = var.db_password
+#   db_subnet_group      = [var.db_subnet_group]
+#   db_security_group_id = var.db_security_group_id 
+# }
